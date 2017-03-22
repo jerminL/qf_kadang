b736f9842d3af86d8440136854fcec36bd809a12
jermin <jiangyoum@sina.cn>
2017-03-22 18:12:58 +0800

    0

--- gulpfile.js
+++ gulpfile.js
@@ -7,29 +7,29 @@ const autoprefixer = require('gulp-autoprefixer')
 
 // 压缩 js
 gulp.task('uglify', function() {
-  gulp.src(['./src/assets/js/*.js'])
+  gulp.src(['./pc-src/assets/js/*.js'])
     .pipe(uglify())
-    .pipe(gulp.dest('./src/dist/js'));
+    .pipe(gulp.dest('./pc-src/dist/js'));
 })
 
 // 编译 sass
 gulp.task('sass', function() {
-  gulp.src(['./src/assets/sass/*.scss'])
+  gulp.src(['./pc-src/assets/sass/*.scss'])
     .pipe(sass().on('error', sass.logError))
     .pipe(autoprefixer())
     .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(gulp.dest('./src/dist/css'))
+    .pipe(gulp.dest('./pc-src/dist/css'))
 })
 
 // 清除文件
 gulp.task('clean', function(cb) {
-  gulp.src(['./src/dist']).pipe(clean())
+  gulp.src(['./pc-src/dist']).pipe(clean())
 })
 
 // 监视文件变动
 gulp.task('watch', function() {
-  gulp.watch(['./src/assets/js/*.js'], ['uglify'])
-  gulp.watch(['./src/assets/sass/*.scss'], ['sass'])
+  gulp.watch(['./pc-src/assets/js/*.js'], ['uglify'])
+  gulp.watch(['./pc-src/assets/sass/*.scss'], ['sass'])
 })
 
 // gulp.task('default', ['uglify', 'sass'])
